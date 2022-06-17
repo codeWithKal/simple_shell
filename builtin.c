c7fb8b8f9967608797c2bc231326c8ccd3b950e7
kalu_coder <awesomecoding51@gmail.com>
2022-06-17 23:31:05 +0300

    functions defined in the header file

--- builtin.c
+++ builtin.c
@@ -29,8 +29,6 @@ int change_dir(sh_t *data)
 		else
 		{
 			SETOWD(data->oldpwd);
-			if (chdir(data->oldpwd) < 0)
-				return (FAIL);
 		}
 	}
 	else
@@ -156,58 +154,4 @@ int isbuiltin(const char *cmd)
 		i++;
 	}
 	return (0);
-}
-
-/**
- * executebuiltin - map a command with a function
- *
- * @tokens: command to execute
- *
- */
-void executebuiltin(char **tokens)
-{
-	builtin_t builtins[] = {
-		{ "exit", __exit },
-		{ "env", _env },
-		{ NULL, NULL }
-	};
-	int i = 0;
-
-	while (builtins[i].cmd)
-	{
-		if (_strcmp(builtins[i].cmd, tokens[0]) == 0)
-			builtins[i].execute_builtin(tokens);
-		i++;
-	}
-}
-
-/**
- * __exit - the exit builtin that exits the shell
- *
- * @tokens: array of character arrays consisting the exit command
- * and its arguments
- *
- * Usage: exit
- *
- */
-void __exit(char **tokens)
-{
-	if (tokens[1])
-		exit(_atoi(tokens[1]));
-	exit(0);
-}
-
-/**
- * _env - the env builtin that prints the current environment
- *
- * @tokens: array of character arrays consisting the env command
- * and its arguments
- *
- */
-void _env(char **tokens)
-{
-	unsigned int i;
-
-	for (i = 0; tokens[0] && environ[i] != NULL; ++i)
-		printf("%s\n", environ[i]);
 }
